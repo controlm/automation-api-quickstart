7c0a00101c3b553e2527631a399d02a441c97a26
nathan-amichay <naamicha@bmc.com>
2016-08-17 14:38:20 +0300

    refactor structure

---
+++ 102-build-docker-containers-for-controlm/alpinelinux-remotehost/run_register_controlm.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+CTM_ENV=endpoint
+CTM_SERVER=[CTM_HOST]
+#CTM_HOSTGROUP=app0
+#CTM_AGENT_PORT=7020
+CTM_AGENT_ALIAS=$(hostname):$CTM_AGENT_PORT
+
+cd
+source .bash_profile
+
+echo run and register controlm agent [$CTM_AGENT_ALIAS] with controlm [$CTM_SERVER], environment [$CTM_ENV] 
+ctm provision setup $CTM_SERVER $CTM_AGENT_ALIAS $CTM_AGENT_PORT -e $CTM_ENV
+
+echo add or create a controlm hostgroup [$CTM_HOSTGROUP] with controlm agent [$CTM_AGENT_ALIAS]
+ctm config server:hostgroup:agent::add $CTM_SERVER $CTM_HOSTGROUP $CTM_AGENT_ALIAS -e $CTM_ENV
+
+# loop forever
+while true; do echo x && sleep 60; done
+
+exit 0
+
