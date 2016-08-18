d2187239410eccc9350aec80f0bbd9e9d5638f8c
nathan-amichay <naamicha@bmc.com>
2016-08-18 18:53:21 +0300

    register remotehost

--- 102-build-docker-containers-for-batch-application/alpinelinux-remotehost/run_register_controlm.sh
+++ 102-build-docker-containers-for-batch-application/alpinelinux-remotehost/run_register_controlm.sh
@@ -3,20 +3,20 @@
 CTM_ENV=endpoint
 #CTM_SERVER=[CTM_HOST]
 #CTM_HOSTGROUP=app0
-#CTM_REMOTHOST_ALIAS=rh
-#$(hostname):$CTM_AGENT_PORT
+#PORT=7020
+#DOCKER_HOST=
+ALIAS=$(DOCKER_HOST):$PORT
 
 cd
 source .bash_profile
 
-echo run and register controlm remotehost [$CTM_REMOTHOST_ALIAS] with controlm [$CTM_SERVER], environment [$CTM_ENV] 
-ctm provision setup $CTM_SERVER $CTM_AGENT_ALIAS $CTM_AGENT_PORT -e $CTM_ENV
+echo register controlm remotehost [$ALIAS] with controlm [$CTM_SERVER], environment [$CTM_ENV]
+config server:remotehost::add $CTM_SERVER $ALIAS $PORT -e $CTM_ENV
 
 echo add or create a controlm hostgroup [$CTM_HOSTGROUP] with controlm agent [$CTM_AGENT_ALIAS]
-ctm config server:hostgroup:agent::add $CTM_SERVER $CTM_HOSTGROUP $CTM_AGENT_ALIAS -e $CTM_ENV
+ctm config server:hostgroup:agent::add $CTM_SERVER $CTM_HOSTGROUP $ALIAS -e $CTM_ENV
 
 # loop forever
-while true; do echo x && sleep 60; done
+#while true; do echo x && sleep 60; done
 
 exit 0
-
