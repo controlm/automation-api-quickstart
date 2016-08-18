93df3a9614743d1c47d6c4126e16794bea0780e3
nathan-amichay <naamicha@bmc.com>
2016-08-18 12:35:49 +0300

    rename docker folder

---
+++ 102-build-docker-containers-for-batch-application/centos7-agent/decommission_controlm.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+CTM_ENV=endpoint
+#CTM_SERVER=[CTM_HOST]
+#CTM_HOSTGROUP=app1 
+#CTM_AGENT_PORT=7020
+CTM_AGENT_ALIAS=$(hostname):$CTM_AGENT_PORT/
+
+cd
+source .bash_profile
+
+echo delete or remove a controlm hostgroup [$CTM_HOSTGROUP] with controlm agent [$CTM_AGENT_ALIAS]
+ctm config server:hostgroup:agent::delete $CTM_SERVER $CTM_HOSTGROUP $CTM_AGENT_ALIAS -e $CTM_ENV
+
+echo stop and unregister controlm agent [$CTM_AGENT_ALIAS] with controlm [$CTM_SERVER], environment [$CTM_ENV] 
+ctm provision uninstall
+
+exit 0
+
