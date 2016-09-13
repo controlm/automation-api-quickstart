1693ad9667df3f5be355dd37de6f991a244dffd5
gad-ron <gron@bmc.com>
2016-09-13 19:31:42 +0300

    fixed . in uri path issue

--- 102-build-docker-containers-for-batch-application/alpinelinux-remotehost/run_register_controlm.sh
+++ 102-build-docker-containers-for-batch-application/alpinelinux-remotehost/run_register_controlm.sh
@@ -5,10 +5,10 @@ CTM_ENV=endpoint
 #CTM_HOSTGROUP=app0
 #PORT=7020
 #DOCKER_HOST=
-ALIAS=$DOCKER_HOST:$PORT/
+ALIAS=$DOCKER_HOST:$PORT
 
 echo register controlm remotehost [$ALIAS] with controlm [$CTM_SERVER], environment [$CTM_ENV]
-ctm config server:remotehost::add $CTM_SERVER "$ALIAS $PORT" -e $CTM_ENV
+ctm config server:remotehost::add $CTM_SERVER "$ALIAS/" $PORT -e $CTM_ENV
 
 echo add or create a controlm hostgroup [$CTM_HOSTGROUP] with controlm agent [$ALIAS]
 ctm config server:hostgroup:agent::add $CTM_SERVER $CTM_HOSTGROUP "$ALIAS" -e $CTM_ENV
