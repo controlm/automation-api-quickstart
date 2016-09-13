b5c3d4aa5e42c91398523789c39e9414d6f0e891
gad-ron <gron@bmc.com>
2016-09-13 19:19:44 +0300

    fixed .in uri path issue

--- 102-build-docker-containers-for-batch-application/alpinelinux-remotehost/decommission_controlm.sh
+++ 102-build-docker-containers-for-batch-application/alpinelinux-remotehost/decommission_controlm.sh
@@ -5,12 +5,12 @@ CTM_ENV=endpoint
 #CTM_HOSTGROUP=app0
 #PORT=7020
 #DOCKER_HOST=
-ALIAS=$DOCKER_HOST:$PORT
+ALIAS=$DOCKER_HOST:$PORT/
 
 echo delete or remove a controlm hostgroup [$CTM_HOSTGROUP] with controlm agent [$ALIAS]
-ctm config server:hostgroup:agent::delete $CTM_SERVER $CTM_HOSTGROUP $ALIAS -e $CTM_ENV
+ctm config server:hostgroup:agent::delete $CTM_SERVER $CTM_HOSTGROUP "$ALIAS" -e $CTM_ENV
 
 echo unregister controlm remotehost [$ALIAS] with controlm [$CTM_SERVER], environment [$CTM_ENV] 
-ctm config server:remotehost::delete $CTM_SERVER $ALIAS
+ctm config server:remotehost::delete $CTM_SERVER "$ALIAS"
 
 exit 0
