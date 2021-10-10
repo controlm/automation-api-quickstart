cb07c866b1682dd48435f5b2b76e848232726688
Galgc <30771919+Galgc@users.noreply.github.com>
2021-10-10 16:25:54 +0300

    Update DeployCode.py
    fixed code to use x-api-key

--- helix-control-m/102-automate-code-deployment/DeployCode.py
+++ helix-control-m/102-automate-code-deployment/DeployCode.py
@@ -1,27 +1,18 @@
 import requests  # pip install requests if you don't have it already
+import urllib3
+
+urllib3.disable_warnings() # disable warnings when creating unverified requests 
   
-endPoint = 'http://<controlMEndPoint>:8443/automation-api'
-  
-user = '<ControlMUser>'
-passwd = '<ControlMPassword>'
-  
-# -----------------
-# login
-r_login = requests.post(endPoint + '/session/login', json={"username": user, "password": passwd})
-print(r_login.content)
-print(r_login.status_code)
-if r_login.status_code != requests.codes.ok:
-    exit(1)
-  
-token = r_login.json()['token']
+endPoint = 'https://<controlmEndPointHost>/automation-api'
+token=<token>
   
 # -----------------
 # Built
 uploaded_files = [
-        ('definitionsFile', ('Jobs.json', open('c:\\src\ctmdk\Jobs.json', 'rb'), 'application/json'))
+        ('definitionsFile', ('Jobs.json', open('c:\\src\Jobs.json', 'rb'), 'application/json'))
 ]
   
-r = requests.post(endPoint + '/deploy', files=uploaded_files, headers={'Authorization': 'Bearer ' + token})
+r = requests.post(endPoint + '/deploy', files=uploaded_files, headers={'x-api-key': + token}, verify=False)
   
 print(r.content)
 print(r.status_code)
