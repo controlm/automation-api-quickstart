f808e213e78d3153970d20bc50af382fbc0f9410
Yosef Peysakhov <ypeysakh@bmc.com>
2023-01-05 13:53:53 +0200

    Update controlm/301-statefulset-agent-to-run-k8s-jobs-using-ai-job with Control-M/Agent 9.0.21.000 support

--- control-m/301-statefulset-agent-to-run-k8s-jobs-using-ai-job/build_docker_example.sh
+++ control-m/301-statefulset-agent-to-run-k8s-jobs-using-ai-job/build_docker_example.sh
@@ -1,10 +1,9 @@
 # This is a build command example with parameters
 # You need to set :
 #   1. AGENT_IMAGE_NAME - the agent image you want to install (taken from "ctm provision images Linux" cli)
-#   2. AI_KUBERNETES_PLUGIN_NAME - the AI Kubernetes plugin name you want to install (taken from AI repo)
-#   3. EXT_AUTO_DEPLOY - a path to a local repository of installation artifacts for provisioning actions performed by the Provision service. It can be folder path (file:///) or URL (https://)
-#   4. EXT_APPLICATION_ARTIFACTS_JSON_URL - a path to a local repository of provisioning images, overriding the location used by default for provisioning actions performed by the Provision service. It can be folder path (file:///) or URL (https://)
-#   5. DOCKER_IMAGE_NAME - the docker image name and optionally a tag in the 'name:tag' format
-sudo docker build --build-arg AGENT_IMAGE_NAME=Agent --build-arg AI_KUBERNETES_PLUGIN_NAME=AI_Kubernetes --build-arg EXT_AUTO_DEPLOY="$EXT_AUTO_DEPLOY" --build-arg EXT_APPLICATION_ARTIFACTS_JSON_URL="$EXT_APPLICATION_ARTIFACTS_JSON_URL" -t "$DOCKER_IMAGE_NAME" docker
+#   2. EXT_AUTO_DEPLOY - a path to a local repository of installation artifacts for provisioning actions performed by the Provision service. It can be folder path (file:///) or URL (https://)
+#   3. EXT_APPLICATION_ARTIFACTS_JSON_URL - a path to a local repository of provisioning images, overriding the location used by default for provisioning actions performed by the Provision service. It can be folder path (file:///) or URL (https://)
+#   4. DOCKER_IMAGE_NAME - the docker image name and optionally a tag in the 'name:tag' format
+sudo docker build --build-arg AGENT_IMAGE_NAME=Agent --build-arg EXT_AUTO_DEPLOY="$EXT_AUTO_DEPLOY" --build-arg EXT_APPLICATION_ARTIFACTS_JSON_URL="$EXT_APPLICATION_ARTIFACTS_JSON_URL" -t "$DOCKER_IMAGE_NAME" docker
 
 # Don't forget to upload the result to your docker repository (ECR, DockerHub, etc.) for k8s use.
