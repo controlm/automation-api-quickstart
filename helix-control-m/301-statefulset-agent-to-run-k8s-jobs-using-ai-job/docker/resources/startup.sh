ea0184c9d98f6c15500d9f363abe1d5e92d77ccc
Peysakhov, Yosef <yosef_peysakhov@bmc.com>
2023-03-30 17:28:29 +0300

    Update best practices for Control-M and Helix

--- helix-control-m/301-statefulset-agent-to-run-k8s-jobs-using-ai-job/docker/resources/startup.sh
+++ helix-control-m/301-statefulset-agent-to-run-k8s-jobs-using-ai-job/docker/resources/startup.sh
@@ -20,7 +20,8 @@ AG_NODE_ID=$(hostname)
 PERSISTENT_VOL=$1/$AG_NODE_ID
 AAPI_END_POINT=$2
 AAPI_TOKEN=$3
-CTM_SERVER_NAME=$4
+AGENT_TOKEN_TAG=$4
+CTM_SERVER_NAME=IN01
 AGENT_HOSTGROUP_NAME=$5
 FOLDERS_EXISTS=false
 AGENT_REGISTERED=false
@@ -49,12 +50,11 @@ if [ ! -d "$PERSISTENT_VOL"/pid ]; then
   mkdir "$PERSISTENT_VOL"
   mv $CONTROLM/backup $CONTROLM/capdef $CONTROLM/dailylog $CONTROLM/data $CONTROLM/measure $CONTROLM/onstmt $CONTROLM/pid $CONTROLM/procid $CONTROLM/status $CONTROLM/sysout $CONTROLM/cm -t "$PERSISTENT_VOL"
 else
-  echo 'this is not the first time an agent is running using this persistent volume, mapping folder to existing persistent volume'
-  FOLDERS_EXISTS=true
-  rm -Rf $CONTROLM/backup $CONTROLM/capdef $CONTROLM/dailylog $CONTROLM/data $CONTROLM/measure $CONTROLM/onstmt $CONTROLM/pid $CONTROLM/procid $CONTROLM/status $CONTROLM/sysout $CONTROLM/cm
+        echo 'this is not the first time an agent is running using this persistent volume, mapping folder to existing persistent volume'
+        FOLDERS_EXISTS=true
+		rm -Rf $CONTROLM/backup $CONTROLM/capdef $CONTROLM/dailylog $CONTROLM/data $CONTROLM/measure $CONTROLM/onstmt $CONTROLM/pid $CONTROLM/procid $CONTROLM/status $CONTROLM/sysout $CONTROLM/cm
   sed '/CM_LIST_SENT2CTMS/d' "$PERSISTENT_VOL"/data/CONFIG.dat
 fi
-
 # create link to persistent volume
 ln -s "$PERSISTENT_VOL"/backup $CONTROLM/backup
 ln -s "$PERSISTENT_VOL"/capdef $CONTROLM/capdef
@@ -73,28 +73,47 @@ ctm env add ctm_env "$AAPI_END_POINT" "$AAPI_TOKEN"
 ctm env set ctm_env
 
 echo "Check if Agent exists in the Control-M Server"
-if $FOLDERS_EXISTS; then
+if $FOLDERS_EXISTS ; then
   ctm config server:agents::get "$CTM_SERVER_NAME" "$AG_NODE_ID" | grep "$AG_NODE_ID"
-  if [[ $? == "0" ]]; then
-    echo 'agent already exists'
-    AGENT_REGISTERED=true
-  fi
+       if [[ $? == "0" ]] ;  then
+	       echo 'agent already exists'
+               AGENT_REGISTERED=true
+       fi
 fi
 
-if $FOLDERS_EXISTS && $AGENT_REGISTERED; then
+if $FOLDERS_EXISTS && $AGENT_REGISTERED ; then
   echo 'Starting the Agent'
-  start-ag -u controlm -p ALL
+               start-ag -u controlm -p ALL
 else
   echo 'Configuring and registering the agent'
-  ctm provision agent::setup "$CTM_SERVER_NAME" "$AG_NODE_ID" 7006 -f agent_configuration.json
+  ctm provision saas:agent::setup $AGENT_TOKEN_TAG $AG_NODE_ID
 fi
 
 echo 'Checking Agent communication with Control-M Server'
 ag_ping
 ag_diag_comm
 
+echo 'Deploying Control-M/Integration Kubernetes Job Type to Control-M/Agent'
+ctm deploy ai:jobtype IN01 "$AG_NODE_ID" KUBERNETES | grep "successful" >res.txt
+
+x=1
+while [[ ! -s res.txt && $x -lt 20 ]]; do
+  echo "attempt #$x"
+  ctm deploy ai:jobtype IN01 "$AG_NODE_ID" KUBERNETES | grep "successful" >res.txt
+  x=$((x + 1))
+  sleep 20
+done
+
+if [[ $x -lt 20 ]]; then
+  echo 'Deploying Control-M/Integration Kubernetes Job Type to Control-M/Agent successes'
+else
+  echo 'Deploying Control-M/Integration Kubernetes Job Type to Control-M/Agent failed'
+fi
+rm res.txt
+
+echo 'KUBERNETES AI Job Type is ready for use'
+
 echo 'Adding the Agent to Host Group'
 ctm config server:hostgroup:agent::add "$CTM_SERVER_NAME" "$AGENT_HOSTGROUP_NAME" "$AG_NODE_ID"
 
-echo 'KUBERNETES AI Job Type is ready for use'
 bash ./ctmhost_keepalive.sh
