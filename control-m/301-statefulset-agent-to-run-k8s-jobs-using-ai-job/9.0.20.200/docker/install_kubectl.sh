92a5ff4f6d438b230f7c1a84c28b63359f26ea3a
sshandor <shoshana_shandorfi@bmc.com>
2025-04-06 15:30:41 +0300

    WBL-315296-Update automation API quick start(git)from Node CLI to Python CLI

--- control-m/301-statefulset-agent-to-run-k8s-jobs-using-ai-job/9.0.20.200/docker/install_kubectl.sh
+++ control-m/301-statefulset-agent-to-run-k8s-jobs-using-ai-job/9.0.20.200/docker/install_kubectl.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-sudo echo "[kubernetes]" | sudo tee -a /etc/yum.repos.d/kubernetes.repo
-sudo echo "name=Kubernetes" | sudo tee -a /etc/yum.repos.d/kubernetes.repo
-sudo echo "baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64" | sudo tee -a /etc/yum.repos.d/kubernetes.repo
-sudo echo "enabled=1" | sudo tee -a /etc/yum.repos.d/kubernetes.repo
-sudo echo "gpgcheck=1" | sudo tee -a /etc/yum.repos.d/kubernetes.repo
-sudo echo "repo_gpgcheck=1" | sudo tee -a /etc/yum.repos.d/kubernetes.repo
-sudo echo "gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg" | sudo tee -a /etc/yum.repos.d/kubernetes.repo
-sudo yum install -y kubectl
+cat <<EOF | tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://pkgs.k8s.io/core:/stable:/v1.28/rpm/
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://pkgs.k8s.io/core:/stable:/v1.28/rpm/repodata/repomd.xml.key
+EOF
+
+microdnf install -y kubectl
